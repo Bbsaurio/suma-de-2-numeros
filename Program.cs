79111b46b5946d27ae339ca40b5bf7e57a45a7c4
sosag <sosag@GAEL>
2025-02-12 16:54:02 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_de_2_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Ingrese el primer numero");
+            int N1= int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Ingrese el segundo numero");
+            int N2= int.Parse(Console.ReadLine());
+            int R= N1 + N2;
+            System.Console.WriteLine("El resultado es:"+R);
+            Console.ReadKey();
+
+        }
+    }
+}
